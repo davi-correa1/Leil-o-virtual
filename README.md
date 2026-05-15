894d5034ce095b419f713af37c32303cf21b2cd2
Davi Corrêa <davi.correa.ribeiro@gmail.com>
2026-05-14 23:44:04 -0300

    Primeiros planejamentos
    Comecei a traçar uma estratégia para fazer este projeto.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Leilão virtual
 Vou fazer leilões virtuais de algumas formas para praticar!
+
+* ## Este codigo será feito com __JS e Node.js__
---
+++ main.js
@@ -0,0 +1,15 @@
+// Global Var
+
+// Prompt user to sign in or sign up
+
+// If sign in
+
+// If sign up
+
+// Interface
+    // 1-Create auction
+    // 2-View yours auctions
+    // 3-View all auctions
+    // 4-LogOut
+
+// Preciso dormir. Amanhã vou continuar a planejar e, talvez, eu comece a codar!
